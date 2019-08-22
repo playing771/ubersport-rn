806747adff7b9a80346db3666ec6ce4dfe3839da
Playing771 <Playing771@gmail.com>
2019-08-23 01:52:31 +0300

    profile tabs added

--- src/screens/EditProfile/UserForm.tsx
+++ src/screens/EditProfile/UserForm.tsx
@@ -13,114 +13,76 @@ import handleApoloError from '../../other/handleApoloError';
 import AvatarSelect from './AvatarSelect';
 
 const UserForm = withEditUserInfoQuery(({ data }) => {
-	const { loading, error, getUser } = data;
+  const { loading, error, getUser } = data;
 
-	const [firstName, setFirstName] = useState('');
-	const [lastName, setLastName] = useState('');
-	const [nickname, setNickname] = useState('');
-	const [sports, setSports] = useState<number[]>([]);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [nickname, setNickname] = useState('');
 
-	useEffect(() => {
-		if (getUser) {
-			setFirstName(getUser.firstName);
-			setLastName(getUser.lastName);
-			setNickname(getUser.nickname);
-			setSports(getUser.favoriteSports.map(sport => sport.id));
-		}
-	}, [getUser]);
+  useEffect(() => {
+    if (getUser) {
+      setFirstName(getUser.firstName);
+      setLastName(getUser.lastName);
+      setNickname(getUser.nickname);
+    }
+  }, [getUser]);
 
-	const changeFirstNameHandle = (text: string) => {
-		setFirstName(text);
-	};
+  const changeFirstNameHandle = (text: string) => {
+    setFirstName(text);
+  };
 
-	const changeLastNameHandle = (text: string) => {
-		setLastName(text);
-	};
+  const changeLastNameHandle = (text: string) => {
+    setLastName(text);
+  };
 
-	const changeNicknameHandle = (text: string) => {
-		setNickname(text);
-	};
+  const changeNicknameHandle = (text: string) => {
+    setNickname(text);
+  };
 
-	const changeSportsHandle = (sportIds: number[]) => {
-		setSports(sportIds);
-	};
+  if (!getUser) {
+    return <ULoader />;
+  }
 
-	if (!getUser) {
-		return <ULoader />;
-	}
+  const updateVariables: IEditProfileVariables = {
+    id: getUser.id,
+    userInput: {
+      firstName,
+      lastName,
+      nickname,
+      // favouriteSportIds: sports
+    },
+  };
 
-	const updateVariables: IEditProfileVariables = {
-		id: getUser.id,
-		userInput: {
-			firstName,
-			lastName,
-			nickname,
-			favouriteSportIds: sports
-		}
-	};
+  console.log('EDIT PROFILE');
 
-	console.log('EDIT PROFILE');
-
-	return (
-		<>
-			<KeyboardAwareScrollView style={styles.mainContainer}>
-				<ULoader loading={loading || !getUser} />
-				<AvatarSelect />
-				<View
-					style={{ backgroundColor: 'white', paddingHorizontal: 24 }}
-				>
-					<UTextInput
-						label="Имя"
-						value={firstName}
-						onChange={changeFirstNameHandle}
-					/>
-					<UTextInput
-						label="Фамилия"
-						value={lastName}
-						onChange={changeLastNameHandle}
-					/>
-					<UTextInput
-						label="Никнейм"
-						value={nickname}
-						onChange={changeNicknameHandle}
-					/>
-					<EditGender options={['Мужской', 'Женский']} label="Пол" />
-					<Text style={styles.sportsListTitle}>
-						Избранные виды спорта
-					</Text>
-					<SportsList
-						onChange={changeSportsHandle}
-						style={styles.sportsList}
-					/>
-				</View>
-			</KeyboardAwareScrollView>
-			<SubmitButton
-				gql={EDIT_USER_MUTATION}
-				variables={updateVariables}
-				title="Сохранить"
-				onError={handleApoloError}
-			/>
-		</>
-	);
+  return (
+    <View style={{ flex: 1 }}>
+      <KeyboardAwareScrollView style={styles.mainContainer}>
+        <ULoader loading={loading || !getUser} />
+        <AvatarSelect />
+        <View style={{ backgroundColor: 'white', paddingHorizontal: 24, paddingBottom: 24 }}>
+          <UTextInput label="Имя" value={firstName} onChange={changeFirstNameHandle} />
+          <UTextInput label="Фамилия" value={lastName} onChange={changeLastNameHandle} />
+          <UTextInput label="Никнейм" value={nickname} onChange={changeNicknameHandle} />
+          <EditGender options={['Мужской', 'Женский']} label="Пол" />
+        </View>
+      </KeyboardAwareScrollView>
+      {/* <SubmitButton
+        gql={EDIT_USER_MUTATION}
+        variables={updateVariables}
+        title="Сохранить"
+        onError={handleApoloError}
+      /> */}
+    </View>
+  );
 });
 
 const styles = StyleSheet.create({
-	mainContainer: {
-		// paddingHorizontal: 24,
-		marginBottom: 48,
-		backgroundColor: '#F1F1F5'
-	},
-	sportsListTitle: {
-		fontSize: 16,
-		fontWeight: '600',
-		textAlign: 'center',
-		color: '#404F7A',
-		paddingBottom: 12,
-		paddingTop: 24
-	},
-	sportsList: {
-		// paddingTop:
-	}
+  mainContainer: {
+    // paddingHorizontal: 24,
+
+    backgroundColor: '#F1F1F5',
+  },
 });
 
 export default UserForm;
--- src/screens/EditProfile/index.tsx
+++ src/screens/EditProfile/index.tsx
@@ -1,23 +1,93 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import useAppContext from '../../hooks/useAppContext';
 import UserForm from './UserForm';
 import UButton from '../../components/UButton';
-import { Ionicons } from '@expo/vector-icons';
-import Colors, { HEADER_BACKGROUND } from '../../constants/Colors';
+import { Ionicons, AntDesign } from '@expo/vector-icons';
+import Colors, { HEADER_BACKGROUND, GREEN_BUTTON_BACKGROUND } from '../../constants/Colors';
 import { NavigationScreenProps } from 'react-navigation';
 import useNavigation from '../../hooks/useNavigation';
+import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
+import { View, StyleSheet, Dimensions, TouchableOpacity, Animated, Text } from 'react-native';
+import SportsList from '../../components/SportsList';
 
 interface IProps extends NavigationScreenProps {}
+
+const FirstRoute = userId => <UserForm id={userId} />;
+
+const SecondRoute = () => <View style={[styles.scene, { backgroundColor: '#673ab7' }]} />;
 
 const EditProfileScreen = (props: IProps) => {
   const { user } = useAppContext();
   const { setParams } = useNavigation();
+  const [currentTabState, setTabState] = useState({
+    index: 0,
+    routes: [
+      { key: 'first', title: 'Мои данные' },
+      { key: 'second', title: 'Cпорт' },
+      { key: 'third', title: 'Прочее' },
+      { key: '4', title: 'Тест4' },
+      { key: '5', title: 'Тест5' },
+      { key: '6', title: 'Тест5' },
+      { key: '7', title: 'Тест5' },
+      { key: '8', title: 'Тест5' },
+    ],
+  });
+
+  const [sports, setSports] = useState<number[]>([]);
 
   useEffect(() => {
     setParams({ test: 'test' });
+    // setSports(getUser.favoriteSports.map(sport => sport.id));
   }, []);
 
-  return <UserForm id={user.id} />;
+  const changeSportsHandle = (sportIds: number[]) => {
+    setSports(sportIds);
+  };
+
+  const _renderTabBar = props => {
+    return (
+      // <View style={styles.tabBar}>
+      <TabBar {...props} scrollEnabled={true}>
+        {props.navigationState.routes.map((route, index) => {
+          return (
+            <TouchableOpacity
+              style={styles.tabItem}
+              onPress={() => setTabState({ ...currentTabState, index })}
+            >
+              <Animated.Text>{route.title}</Animated.Text>
+            </TouchableOpacity>
+          );
+        })}
+      </TabBar>
+    );
+  };
+
+  return (
+    <View style={{ flex: 1 }}>
+      <TabView
+        navigationState={currentTabState}
+        renderScene={SceneMap({
+          first: () => <UserForm id={user.id} />,
+          second: () => (
+            <>
+              <Text style={styles.sportsListTitle}>Избранные виды спорта</Text>
+              <SportsList onChange={changeSportsHandle} style={styles.sportsList} />
+            </>
+          ),
+          third: SecondRoute,
+          4: SecondRoute,
+          5: SecondRoute,
+          6: SecondRoute,
+          7: SecondRoute,
+          8: SecondRoute,
+        })}
+        renderTabBar={_renderTabBar}
+        onIndexChange={index => setTabState({ ...currentTabState, index })}
+        initialLayout={{ width: Dimensions.get('window').width }}
+        style={{ flex: 1 }}
+      />
+    </View>
+  );
 };
 
 EditProfileScreen.navigationOptions = ({ navigation }) => {
@@ -50,10 +120,40 @@ function HeaderRightButton({ onPress }: { onPress: () => void }) {
       // iconStyle={{ width: 20, height: 20 }}
       backgroundColor="transparent"
       style={{ marginRight: 12 }}
+      title="Cохранить"
+      textStyle={{ fontSize: 18 }}
     >
-      <Ionicons name="ios-log-out" size={30} color={Colors.purle} />
+      {/* <Ionicons name="ios-log-out" size={30} color={Colors.purle} /> */}
+      {/* <Ionicons name="ios-save" size={30} color={'white'} /> */}
+      {/* <AntDesign size={24} color={'white'} name="save"></AntDesign>  */}
     </UButton>
   );
 }
+
+const styles = StyleSheet.create({
+  scene: {
+    flex: 1,
+  },
+  tabBar: {
+    flexDirection: 'row',
+    // paddingTop: Constants.statusBarHeight,
+  },
+  tabItem: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 16,
+  },
+  sportsListTitle: {
+    fontSize: 16,
+    fontWeight: '600',
+    textAlign: 'center',
+    color: '#404F7A',
+    paddingBottom: 12,
+    paddingTop: 24,
+  },
+  sportsList: {
+    // paddingTop:
+  },
+});
 
 export default EditProfileScreen;
