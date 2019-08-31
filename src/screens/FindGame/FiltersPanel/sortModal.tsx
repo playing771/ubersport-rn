41a7cfc240001201849ebeb028c9589fc3c29b52
Playing771 <Playing771@gmail.com>
2019-08-31 22:03:05 +0300

    sort modal refactoring

--- src/screens/FindGame/FiltersPanel/sortModal.tsx
+++ src/screens/FindGame/FiltersPanel/sortModal.tsx
@@ -1,38 +1,30 @@
-import React from "react";
-import { Text, StyleSheet } from "react-native";
-import Colors from "../../../constants/Colors";
-import { ISearchGameSort } from "../../FindGame";
-import ToggleableItem from "../../../components/ToggleableItem";
-import UModal from "../../../components/UModal";
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+import Colors from '../../../constants/Colors';
+import { ISearchGameSort } from '../../FindGame';
+import ToggleableItem from '../../../components/ToggleableItem';
+import UModal from '../../../components/UModal';
 
 interface IProps {
   onChange: (itemId: ISearchGameSort) => void;
   activeSort: ISearchGameSort;
 }
 
-const SortModal: React.FC<IProps> = props => {
+const SortModal = (props: IProps) => {
   return (
     <UModal>
       <Text style={styles.header}>Сортировка</Text>
       <ToggleableItem
         onPress={props.onChange}
         itemId="distance"
-        active={props.activeSort === "distance"}
+        active={props.activeSort === 'distance'}
       >
         Ближайшие
       </ToggleableItem>
-      <ToggleableItem
-        onPress={props.onChange}
-        itemId="time"
-        active={props.activeSort === "time"}
-      >
+      <ToggleableItem onPress={props.onChange} itemId="time" active={props.activeSort === 'time'}>
         Скоро начало
       </ToggleableItem>
-      <ToggleableItem
-        onPress={props.onChange}
-        itemId="date"
-        active={props.activeSort === "date"}
-      >
+      <ToggleableItem onPress={props.onChange} itemId="date" active={props.activeSort === 'date'}>
         Новые
       </ToggleableItem>
     </UModal>
@@ -41,30 +33,30 @@ const SortModal: React.FC<IProps> = props => {
 
 const styles = StyleSheet.create({
   header: {
-    color: "#667286",
+    color: '#667286',
     paddingVertical: 12,
     paddingBottom: 18,
     fontSize: 16,
-    fontWeight: "500",
-    textAlign: "center"
+    fontWeight: '500',
+    textAlign: 'center',
   },
   item: {
     height: 50,
-    flexDirection: "row",
-    alignItems: "center",
-    paddingVertical: 12
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 12,
   },
   itemText: {
-    color: "#667286",
-    fontWeight: "700",
-    fontSize: 14
+    color: '#667286',
+    fontWeight: '700',
+    fontSize: 14,
   },
   itemIcon: {
-    marginLeft: "auto"
+    marginLeft: 'auto',
   },
   active: {
-    color: Colors.active
-  }
+    color: Colors.active,
+  },
 });
 
 export default SortModal;
