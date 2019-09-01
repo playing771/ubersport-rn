3329fdcc421e25fc2b372fb5e38a69f7d25157e1
Playing771 <Playing771@gmail.com>
2019-09-01 22:26:07 +0300

    profile refactoring

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -8,13 +8,14 @@ import { GameStatus } from '../../api/games/types';
 
 import { NavigationRoot } from '../../navigation/roots';
 import useNavigation from '../../hooks/useNavigation';
-import UButton from '../../components/UButton';
 
-type IProps = { userId: string };
+interface IProps {
+  userId: string;
+}
 
 const Profile = ({ userId }: IProps) => {
   const { navigate } = useNavigation();
-  const _onGameCardPress = (gameId: string) => {
+  const onGameCardPress = (gameId: string) => {
     navigate(NavigationRoot.GameInfo, { gameId });
   };
 
@@ -24,14 +25,14 @@ const Profile = ({ userId }: IProps) => {
       <View style={styles.subContainer}>
         <ProfileGamesList
           userId={userId}
-          onGamePress={_onGameCardPress}
+          onGamePress={onGameCardPress}
           status={GameStatus.Pending}
           title="Активные игры"
           emptyText="Активных игр нет"
         />
         <ProfileGamesList
           userId={userId}
-          onGamePress={_onGameCardPress}
+          onGamePress={onGameCardPress}
           status={GameStatus.Finished}
           title="История игр"
           emptyText="Список истории игр пуст"
@@ -39,35 +40,6 @@ const Profile = ({ userId }: IProps) => {
       </View>
     </ScrollView>
   );
-};
-
-Profile.navigationOptions = {
-  title: 'TITLE',
-  headerTitleStyle: {
-    color: '#fff',
-    fontWeight: '400',
-    fontSize: 22,
-  },
-  headerTransparent: true, // TODO: fix
-  headerRight: (
-    // <View
-    //   style={{
-    //     paddingHorizontal: 6,
-    //     flexDirection: 'row'
-    //     // justifyContent: 'center',
-    //     // width: '100%'
-    //   }}
-    // >
-    <UButton
-      onPress={() => alert('This is a button!')}
-      icon="ios-search"
-      iconColor="#dcdcdc"
-      // iconStyle={{ width: 20, height: 20 }}
-      iconSize={24}
-      backgroundColor="transparent"
-      style={{ width: 40, height: 40, marginRight: 10 }}
-    />
-  ),
 };
 
 const styles = StyleSheet.create({
