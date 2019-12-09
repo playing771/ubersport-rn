03d5de92333e3be9b7b0541449265a18a9f68e15
Playing771 <Playing771@gmail.com>
2019-12-09 23:21:31 +0300

    api fix

--- src/constants/Api.tsx
+++ src/constants/Api.tsx
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://192.168.0.186:3000';
-// export const BASE_URL = 'https://ubersport.ru';
+// export const BASE_URL = 'http://192.168.0.186:3000';
+export const BASE_URL = 'https://ubersport.ru';
 export const FILES_URL = `${BASE_URL}/file`;
