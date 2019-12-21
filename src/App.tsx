5bae534576bf7c4e907d196c36a676b6b7cf85bc
Narek Garsantsyan <gnarek.a@gmail.com>
2019-12-22 00:45:02 +0300

    add expo notifications stage 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useCallback } from 'react';
-import { StyleSheet, View, Platform, StatusBar, AppRegistry } from 'react-native';
+import React, { useState, useCallback, useEffect } from 'react';
+import { StyleSheet, View, Platform, StatusBar, AppRegistry, Text } from 'react-native';
 import { ApolloProvider } from '@apollo/react-hooks';
-import { AppLoading } from 'expo';
+import { AppLoading, Notifications } from 'expo';
 import * as Font from 'expo-font';
 import { createAppContainer } from 'react-navigation';
 
@@ -9,6 +9,7 @@ import { AppContextProvider, uknonwUser } from './utils/context/sports';
 import AppNavigator from './navigation/AppNavigator';
 import { IUserWithToken } from './api/user/types';
 import { client } from './client';
+import registerForPushNotificationsAsync from './notifiactions';
 
 const NavigatorWrapper = createAppContainer(AppNavigator);
 
@@ -20,12 +21,30 @@ export default function App(props: IProps) {
   const { skipLoadingScreen } = props;
   const [isLoadingComplete, setLoadingComplete] = useState(false);
   const [user, setUser] = useState(uknonwUser);
+  const [notification, setNotifications] = useState({}) as any;
 
+  useEffect(() => {
+    let _notificationSubscription;
+    registerForPushNotificationsAsync();
+
+    // Handle notifications that are received or selected while the app
+    // is open. If the app was closed and then opened by tapping the
+    // notification (rather than just tapping the app icon to open it),
+    // this function will fire on the next tick after the app starts
+    // with the notification data.
+    _notificationSubscription = Notifications.addListener(handleNotification);
+  });
+  const handleNotification = (notification: any) => {
+    setNotifications(notification);
+    console.log('NOTIFACTION', notification);
+    setTimeout(() => setNotifications({}), 2000);
+  };
   const setLoggedInUserHandle = (userInput: IUserWithToken) => {
     setUser(userInput);
   };
   const handleFinishLoading = () => {
     setLoadingComplete(true);
+    registerForPushNotificationsAsync();
   };
 
   const loadResourcesAsync = async () => {
@@ -62,6 +81,21 @@ export default function App(props: IProps) {
         <View style={styles.container}>
           {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
           <NavigatorWrapper />
+          {notification.origin && (
+            <View
+              style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: 'red',
+              }}
+            >
+              <Text>Origin: {notification && notification.origin}</Text>
+              <Text>
+                Data: {JSON.stringify(notification && notification.data && notification.data.body)}
+              </Text>
+            </View>
+          )}
         </View>
       </AppContextProvider>
     </ApolloProvider>
